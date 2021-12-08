c9b5c3a2b4eb137be7acfe00dd920f51258ad263
HardreaM <46752979+HardreaM@users.noreply.github.com>
2021-12-08 22:23:02 +0500

    Add files via upload

--- songs.py
+++ songs.py
@@ -1,22 +1,33 @@
-from logging import error
 import os
 import eyed3
 from sys import argv
-import time
 
 def main ():
 
-    target_dir, artist = argv[1::]
-    target_dir = validate_vals(target_dir)
-    artist = validate_vals(artist)
+    if len(argv) == 3:
+        target_dir, artist = argv[1::]
+        target_dir = validate_vals(target_dir)
+        artist = validate_vals(artist)
+        album = artist
+    elif len(argv) == 4:
+        target_dir, artist, album = argv[1::]
+        target_dir = validate_vals(target_dir)
+        artist = validate_vals(artist)
+        album = validate_vals(album)
 
     try:
         for song in os.listdir(target_dir):
             if ".mp3" in song:
+                name = song.replace('.mp3', '')
                 song = eyed3.load(target_dir + song)
-                song.tag.artist = artist
-                song.tag.album = artist
-                song.tag.album_artist = artist
+
+                if song.tag.artist != artist or song.tag.album != artist or song.tag.album_artist != artist:
+                    song.tag.artist = artist
+                    song.tag.album = album
+                    song.tag.album_artist = artist
+
+                if song.tag.title != name:
+                    song.tag.title = name
 
                 song.tag.save()
 
@@ -31,7 +42,7 @@ def validate_vals (value):
 
 
 if __name__ == "__main__":
-    if "--help" in argv or len(argv) != 3:
-        print("Usage: \n>songs.py {target_dir} {artist_name} \nexample: >songs.py C:/Music/Manowar/ Manowar")
+    if "--help" in argv or (len(argv) != 3 and len(argv) != 4):
+        print("Tool for redacting metadata\n\nUsage: \n>songs.py {target_dir} {artist_name} {album_name} \n\nExample: \n>songs.py C:/Music/Manowar/ Manowar \"Kings of Metal\"")
     else:
         main()
